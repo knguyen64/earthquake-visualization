2adfd06ddb45f6eda1c8fba09ea3349605be9231
Kathleen Nguyen <kathleennguyen@Kathleens-MacBook-Pro.local>
2024-02-15 16:06:27 -0800

    added data

---
+++ src/components/App.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {onMount} from svelte
+    import * as d3 from 'd3';
+    
+    let quakeData = [];
+
+    onMount(async () => {
+
+        const res = await fetch('earthquake_states.csv'); 
+
+        const csv = await res.text();
+
+        quakeData = d3.csvParse(csv, d3.autoType)
+
+        console.log(tempData);
+
+    });
+</script>
